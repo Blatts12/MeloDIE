2b3743af1b6f61c42e76b42f7f98ba172dee6635
Jakub <9402720+Blatts12@users.noreply.github.com>
2021-09-08 13:32:39 +0200

    remove try blocks

--- Project/Database/Database.py
+++ Project/Database/Database.py
@@ -1,37 +1,31 @@
 import sqlite3
 from pathlib import Path
 
-dbPath = "C:/_Muzyka/pls.db"
-# dbPath = str(Path.home()) + "/Music/pls.db"
+# dbPath = "C:/_Muzyka/pls.db"
+dbPath = str(Path.home()) + "/Music/pls.db"
 
 
-class PlaylistDatabase():
+class PlaylistDatabase:
     def __init__(self):
         with sqlite3.connect(dbPath) as conn:
             c = conn.cursor()
             c.execute(
-                "CREATE TABLE IF NOT EXISTS Playlists(id INTEGER PRIMARY KEY, name TEXT NOT NULL, link TEXT NOT NULL UNIQUE)")
+                "CREATE TABLE IF NOT EXISTS Playlists(id INTEGER PRIMARY KEY, name TEXT NOT NULL, link TEXT NOT NULL UNIQUE)"
+            )
             conn.commit()
 
     def addPlaylist(self, name, link):
         with sqlite3.connect(dbPath) as conn:
-            try:
-                playlist = [(name, link)]
-                c = conn.cursor()
-                c.executemany(
-                    "INSERT INTO Playlists (name, link) VALUES (?,?)", playlist)
-                conn.commit()
-            except:
-                pass
+            playlist = [(name, link)]
+            c = conn.cursor()
+            c.executemany("INSERT INTO Playlists (name, link) VALUES (?,?)", playlist)
+            conn.commit()
 
     def removePlaylist(self, link):
         with sqlite3.connect(dbPath) as conn:
-            try:
-                c = conn.cursor()
-                c.execute("DELETE FROM Playlists WHERE link=?", (link,))
-                conn.commit()
-            except:
-                pass
+            c = conn.cursor()
+            c.execute("DELETE FROM Playlists WHERE link=?", (link,))
+            conn.commit()
 
     def getPlaylists(self):
         with sqlite3.connect(dbPath) as conn:
